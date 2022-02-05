5591f66fc76ce8ed1571c5e16cf831beaba01bc2
harish <yandrapragadaharish@gmail.com>
2022-02-05 12:46:23 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/firstapp/authapp/PackageContext.java
@@ -0,0 +1,4 @@
+package com.firstapp.authapp;
+
+public class PackageContext {
+}
---
+++ app/src/main/java/com/firstapp/authapp/User.java
@@ -0,0 +1,9 @@
+package com.firstapp.authapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User {
+    public String fullName,age,email;
+}
